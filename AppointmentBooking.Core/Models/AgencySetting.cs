a4eae0d0a0213892d09cb5496e14f09a6fe2e257
Ekaby <fauziahoji95@gmail.com>
2025-12-26 21:23:28 +0700

    create model

---
+++ AppointmentBooking.Core/Models/AgencySetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentBooking.Core.Models
+{
+    public class AgencySetting
+    {
+        public int Id { get; set; }
+        public int MaxAppointmentsPerDay { get; set; }
+    }
+}
---
+++ AppointmentBooking.Core/Models/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentBooking.Core.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public int TokenNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
